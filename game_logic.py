920865683a4a5533e8d2c49b269098ddb50b6994
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2024-03-12 12:36:47 +0200

    tkinter gui, game and game tree

---
+++ game_logic.py
@@ -0,0 +1,111 @@
+import random
+
+class Node:
+    def __init__(self, number, score, game_bank, depth, player):
+        self.number = number
+        self.score = score
+        self.game_bank = game_bank
+
+        self.depth = depth
+        self.player = player
+
+        self.children = []
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def is_leaf(self):
+        return self.number % 3 != 0 and self.number % 4 != 0 and self.number % 5 != 0
+
+    def other_player(self):
+        return "human" if self.player == "computer" else "computer"
+    
+    def divide_by_3(self):
+        if self.number % 3 == 0:
+            new_number = self.number // 3
+            new_score = self.score + 1 if new_number % 2 == 0 else self.score - 1
+            new_game_bank = self.game_bank + 1 if new_number % 5 == 0 else self.game_bank
+            return Node(new_number, new_score, new_game_bank, self.depth + 1, self.other_player())
+        return None
+
+    def divide_by_4(self):
+        if self.number % 4 == 0:
+            new_number = self.number // 4
+            new_score = self.score + 1 if new_number % 2 == 0 else self.score - 1
+            new_game_bank = self.game_bank + 1 if new_number % 5 == 0 else self.game_bank
+            return Node(new_number, new_score, new_game_bank, self.depth + 1, self.other_player())
+        return None
+
+    def divide_by_5(self):
+        if self.number % 5 == 0:
+            new_number = self.number // 5
+            new_score = self.score + 1 if new_number % 2 == 0 else self.score - 1
+            new_game_bank = self.game_bank + 1 if new_number % 5 == 0 else self.game_bank
+            return Node(new_number, new_score, new_game_bank, self.depth + 1, self.other_player())
+        return None
+
+
+class Game:
+    @staticmethod
+    def generate_start_numbers():
+        numbers = []
+        for _ in range(5):
+            number = random.randint(40000, 50000)
+            while (number in numbers) or (number % 3 != 0) or (number % 4 != 0) or (number % 5 != 0):
+                number = random.randint(40000, 50000)
+            numbers.append(number)
+        return numbers
+    
+    def __init__(self, first_player, algorithm, initial_selected_number):
+        other_player = "computer" if first_player == "human" else "human"
+        self.algorithm = algorithm
+        self.state = Node(initial_selected_number, 0, 0, 0, other_player)
+
+    def create_tree(self):
+        queue = [self.state]
+        while queue:
+            current_node = queue.pop(0)
+            children = [current_node.divide_by_3(), current_node.divide_by_4(), current_node.divide_by_5()]
+            for child in children:
+                if child:
+                    current_node.add_child(child)
+                    queue.append(child)
+
+    def print_tree(self):
+        queue = [self.state]
+        current_depth = 0
+        print("Depth 0 (root):     ", end="")
+        while queue:
+            current_node = queue.pop(0)
+            if current_node.depth > current_depth:
+                current_depth = current_node.depth
+                print(f"\nDepth {current_depth} ({current_node.player}): ", end="")
+                if current_node.player == "human":
+                    print("   ", end="")
+            print(f"({current_node.number}|{current_node.score}|{current_node.game_bank}) ", end="")
+            for child in current_node.children:
+                queue.append(child)
+        print()
+
+    def play(self, number):
+        new_state = None
+        if number == 3:
+            new_state = self.state.divide_by_3()
+        elif number == 4:
+            new_state = self.state.divide_by_4()
+        elif number == 5:
+            new_state = self.state.divide_by_5()
+        new_state.depth = 0
+        self.state = new_state
+
+    def get_computer_random_play(self):
+        choice = random.choice([3, 4, 5])
+        while self.state.number % choice != 0:
+            choice = random.choice([3, 4, 5])
+        return choice
+    
+    def get_computer_play(self):
+        if self.algorithm == "minimax":
+            return self.minimax() # TO IMPLEMENT
+        elif self.algorithm == "alpha-beta":
+            return self.alpha_beta() # TO IMPLEMENT
--- main.py
+++ main.py
@@ -1,11 +1,168 @@
-import ai_algorithm
 import game_logic
-import gui
 
 import tkinter as tk
+import time
+
+def create_gui():
+    global root
+    root = tk.Tk()
+    root.title("Divide by 3, 4, 5 Game")
+    root.geometry("700x700")
+    root.resizable(False, False)
+    window_width = root.winfo_reqwidth()
+    window_height = root.winfo_reqheight()
+    position_right = int(root.winfo_screenwidth()/2 - window_width/2 - 700/2)
+    position_down = int(root.winfo_screenheight()/2 - window_height/2 - 700/2)
+    root.geometry("+{}+{}".format(position_right, position_down))
+    return root
+
+def clear_frame():
+    for widget in root.winfo_children():
+        widget.destroy()
+
+def show_config(numbers):
+    first_player = "human"
+    algorithm = "minimax"
+    initial_selected_number = numbers[0]
+
+    def select_first_player(player):
+        nonlocal first_player
+        first_player = player
+        if player == "human":
+            button_config_human.config(bg="blue", fg="white")
+            button_config_computer.config(bg="#d9d9d9", fg="black")
+        else:
+            button_config_human.config(bg="#d9d9d9", fg="black")
+            button_config_computer.config(bg="blue", fg="white")
+
+    def select_algorithm(algo):
+        nonlocal algorithm
+        algorithm = algo
+        if algo == "minimax":
+            button_config_minimax.config(bg="blue", fg="white")
+            button_config_alpha.config(bg="#d9d9d9", fg="black")
+        else:
+            button_config_minimax.config(bg="#d9d9d9", fg="black")
+            button_config_alpha.config(bg="blue", fg="white")
+
+    def select_number(number):
+        nonlocal initial_selected_number
+        initial_selected_number = number
+        for btn in buttons_config_number:
+            if btn["text"] == str(number):
+                btn.config(bg="blue", fg="white")
+            else:
+                btn.config(bg="#d9d9d9", fg="black")
+
+    top_space = tk.Frame(root, height=75)
+    top_space.pack()
+    tk.Label(root, text="Select the first player:").pack()
+    button_frame = tk.Frame(root)
+    button_frame.pack(pady=20)
+    button_config_human = tk.Button(button_frame, text="Human", bg="blue", fg="white", command=lambda: select_first_player("human"))
+    button_config_human.pack(side=tk.LEFT, padx=5, pady=5)
+    button_config_computer = tk.Button(button_frame, text="Computer", command=lambda: select_first_player("computer"))
+    button_config_computer.pack(side=tk.LEFT, padx=5, pady=5)
+
+    tk.Label(root, text="Select the algorithm:").pack()
+    button_frame = tk.Frame(root)
+    button_frame.pack(pady=20)
+    button_config_minimax = tk.Button(button_frame, text="Minimax", bg="blue", fg="white", command=lambda: select_algorithm("minimax"))
+    button_config_minimax.pack(side=tk.LEFT, padx=5, pady=5)
+    button_config_alpha = tk.Button(button_frame, text="Alpha-beta pruning", command=lambda: select_algorithm("alpha-beta"))
+    button_config_alpha.pack(side=tk.LEFT, padx=5, pady=5)
+
+    tk.Label(root, text="Select a number:").pack()
+    button_frame = tk.Frame(root)
+    button_frame.pack(pady=20)
+    buttons_config_number = [tk.Button(button_frame, text=str(number), command=lambda num=number: select_number(num)) for number in numbers]
+    for btn in buttons_config_number:
+        btn.pack(side=tk.LEFT, padx=5, pady=5)
+        if btn["text"] == str(initial_selected_number):
+            btn.config(bg="blue", fg="white")
+
+    button_frame = tk.Frame(root)
+    button_frame.pack(pady=20)
+    button_config_start = tk.Button(button_frame, text="Start", bg="green", fg="white", command=lambda: start_game(first_player, algorithm, initial_selected_number))
+    button_config_start.pack(side=tk.LEFT, padx=5, pady=5)
+
+def start_game(first_player, algorithm, initial_selected_number):
+    global game
+    game = game_logic.Game(first_player, algorithm, initial_selected_number)
+    print("First player:", first_player)
+    print("Algorithm:", algorithm)
+    print("Initial selected number:", initial_selected_number)
+    print("========== Initial game tree (number|score|game_bank) ==========")
+    game.create_tree()
+    game.print_tree()
+
+    clear_frame()
+    top_space = tk.Frame(root, height=50)
+    top_space.pack()
+    number_label = tk.Label(root, text=f"Number: {game.state.number}", font=("Arial", 20))
+    number_label.pack()
+    score_label = tk.Label(root, text=f"Score: {game.state.score}")
+    score_label.pack()
+    bank_label = tk.Label(root, text=f"Game bank: {game.state.game_bank}")
+    bank_label.pack()
+    top_space = tk.Frame(root, height=50)
+    top_space.pack()
+    button_divide_by_3 = tk.Button(root, text="Divide by 3", command=lambda: human_play(3, number_label, score_label, bank_label, button_divide_by_3, button_divide_by_4, button_divide_by_5))
+    button_divide_by_3.pack(pady=20)
+    if game.state.number % 3 != 0:
+        button_divide_by_3.config(state=tk.DISABLED)
+    button_divide_by_4 = tk.Button(root, text="Divide by 4", command=lambda: human_play(4, number_label, score_label, bank_label, button_divide_by_3, button_divide_by_4, button_divide_by_5))
+    button_divide_by_4.pack(pady=20)
+    if game.state.number % 4 != 0:
+        button_divide_by_4.config(state=tk.DISABLED)
+    button_divide_by_5 = tk.Button(root, text="Divide by 5", command=lambda: human_play(5, number_label, score_label, bank_label, button_divide_by_3, button_divide_by_4, button_divide_by_5))
+    button_divide_by_5.pack(pady=20)
+    if game.state.number % 5 != 0:
+        button_divide_by_5.config(state=tk.DISABLED)
+
+def human_play(divisor, number_label, score_label, bank_label, button_divide_by_3, button_divide_by_4, button_divide_by_5):
+    global game
+    game.play(divisor)
+    number_label.config(text=f"Number: {game.state.number}")
+    score_label.config(text=f"Score: {game.state.score}")
+    bank_label.config(text=f"Game bank: {game.state.game_bank}")
+    button_divide_by_3.config(state=tk.DISABLED)
+    button_divide_by_4.config(state=tk.DISABLED)
+    button_divide_by_5.config(state=tk.DISABLED)
+    print(f"========== Game tree after human's move (divide by {divisor})==========")
+    game.create_tree()
+    game.print_tree()
+    if game.state.is_leaf():
+        game_over(game.state.score, game.state.game_bank)
+        return
+    computer_play(number_label, score_label, bank_label, button_divide_by_3, button_divide_by_4, button_divide_by_5)
+    
+def computer_play(number_label, score_label, bank_label, button_divide_by_3, button_divide_by_4, button_divide_by_5):
+    global game
+    time.sleep(2)
+    divisor = game.get_computer_random_play() # TO CHANGE by "divisor = game.get_computer_play()" when the ai algorithms are implemented
+    game.play(divisor)
+    number_label.config(text=f"Number: {game.state.number}")
+    score_label.config(text=f"Score: {game.state.score}")
+    bank_label.config(text=f"Game bank: {game.state.game_bank}")
+    print(f"========== Game tree after computer's move (divide by {divisor})==========")
+    game.create_tree()
+    game.print_tree()
+    if game.state.is_leaf():
+        game_over(game.state.score, game.state.game_bank)
+        return
+    button_divide_by_3.config(state=tk.NORMAL if game.state.number % 3 == 0 else tk.DISABLED)
+    button_divide_by_4.config(state=tk.NORMAL if game.state.number % 4 == 0 else tk.DISABLED)
+    button_divide_by_5.config(state=tk.NORMAL if game.state.number % 5 == 0 else tk.DISABLED)
+
+def game_over(score, game_bank):
+    pass
 
 def main():
-    
+    root = create_gui()
+    start_numbers = game_logic.Game.generate_start_numbers()
+    show_config(start_numbers)
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
